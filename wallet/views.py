0cfa132a3f8f4cc68805bcf97e42c46ddc75b9e2
SG7R <SG7R77@gmail.com>
2025-03-31 23:44:50 -0500

    Changed logic -edit record button- to do nothinh with records that posted by admin

--- wallet/views.py
+++ wallet/views.py
@@ -42,6 +42,26 @@ class AnonymousList(viewsets.ModelViewSet):
 
         return Response(status=status.HTTP_403_FORBIDDEN)
 
+   # Logic for update if not logged in
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        if instance.user_id is None:
+
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
+
+            return Response(serializer.data)
+
+        return Response(status=status.HTTP_403_FORBIDDEN)
+
 
 class WalletList(APIView):
     permission_classes = [IsAuthenticated]
